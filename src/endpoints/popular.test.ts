0ce79b1ae7c45635e5677edab387258e973ef37b
Eric Hicks <ehicks05@yahoo.com>
2025-04-22 16:17:13 -0400

    test

---
+++ src/endpoints/popular.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest';
+import { tmdb } from '../tests/client.js';
+
+const pages = 2;
+
+test('get providers', async () => {
+	const popularPeople = await tmdb.popular({ resource: 'person', pages });
+	const p0 = popularPeople[0];
+	const p39 = popularPeople[39];
+
+	expect(popularPeople).length(pages * 20);
+	expect(p0).toBeDefined();
+	expect(p39).toBeDefined();
+	expect(p0.popularity).greaterThanOrEqual(p39.popularity);
+});
